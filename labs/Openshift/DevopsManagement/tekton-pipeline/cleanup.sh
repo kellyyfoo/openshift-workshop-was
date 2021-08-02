4e5e51bc5ae2bc647ff90aafc7e15f96297cb0d0
ibmuser <ibmuser@admin.ibm.demo>
2021-08-02 12:33:05 -0400

    revert

--- labs/Openshift/DevopsManagement/tekton-pipeline/cleanup.sh
+++ labs/Openshift/DevopsManagement/tekton-pipeline/cleanup.sh
@@ -3,7 +3,7 @@ oc delete -f pipeline-deploy.yaml
 oc delete -f buildah.yaml
 oc delete -f oc-deploy-klp.yaml
 
-oc delete -f  ../deploy/base/olapp-cos.yaml
+oc delete -f  deploy/base/olapp-cos.yaml
 oc delete configmap cos-config
 oc delete secret db-creds
 oc delete secret liberty-creds
