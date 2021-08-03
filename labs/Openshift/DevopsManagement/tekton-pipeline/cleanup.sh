af163962391c278db578b2e5412b5de7f87669cd
ibmuser <ibmuser@admin.ibm.demo>
2021-08-03 12:04:53 -0400

    fixed workspace paths

--- labs/Openshift/DevopsManagement/tekton-pipeline/cleanup.sh
+++ labs/Openshift/DevopsManagement/tekton-pipeline/cleanup.sh
@@ -1,17 +1,15 @@
 oc delete -f pipeline-deploy-run.yaml
 oc delete -f pipeline-deploy.yaml
 oc delete -f buildah.yaml
-oc delete -f oc-deploy-klp.yaml
+oc delete -f oc-deploy.yaml
+oc delete -f git-clone.yaml
 
 oc delete -f  ../deploy/base/olapp-cos.yaml
 oc delete configmap cos-config
 oc delete secret db-creds
 oc delete secret liberty-creds
 
-oc delete -f img-resource.yaml
-oc delete -f git-resource.yaml
 
-#oc delete -f run-task-deploy.yaml 
 
 
 
